7c27b0d096fc92fc9f54001dfd4557d2f8744cfa
TheDeafOne <woodburnkb20@gcc.edu>
2022-05-17 17:55:23 -0400

    CircularBuffer variables and method headers

--- LinearBased/Arrays/CircularBuffer/CircularBuffer.h
+++ LinearBased/Arrays/CircularBuffer/CircularBuffer.h
@@ -4,11 +4,44 @@
 
 class CircularBuffer {
     private:
-       
+        // array size
+        size_t buffer_size;
 
+        // buffer
+        int* buffer;
+
+        // reader and writer (sometimes referred to as start and end respectively)
+        int reader = 0;
+        int writer = 0;
 
     public:
+        /**
+         * @brief Construct a new Circular Buffer object
+         * 
+         * @param size length of buffer
+         */
+        CircularBuffer(size_t size);
 
-}
+        /**
+         * @brief set the current write index to a given value
+         * 
+         * @param value given int to add to buffer
+         */
+        void write(int value);
+
+        /**
+         * @brief read the int at the current read index
+         * 
+         * @return int value at read index
+         */
+        int read();
+
+        /**
+         * @brief returns size of buffer
+         * 
+         * @return size_t size of buffer
+         */
+        size_t size() { return buffer_size; }
+};
 
 #endif
