a0813dffe04773b1fb7947cf6a3a6ca6f36a9cb2
TheDeafOne <woodburnkb20@gcc.edu>
2022-05-18 19:03:03 -0400

    CircularBuffer documentation

---
+++ LinearBased/Arrays/CircularBuffer/CircularBufferTutorial.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "CircularBuffer.h"
+using namespace std;
+
+void printBuffer(CircularBuffer buffer) {
+    cout << "[";
+    for (int i = 0; i < 9; i++){ // reading only 9 values so a comma isn't placed after the 10th 
+        cout << buffer.read() << ", ";
+    }
+    cout << buffer.read() << "]" << endl;
+}
+
+int main() {
+    CircularBuffer buffer(10);
+    cout << "we can fill a buffer with the write function and print it using the read function:" << endl;
+    for (int i = 0; i < 10; i++){
+        buffer.write(i);
+    }
+    printBuffer(buffer);
+
+    cout << "once we write another value to the buffer, the value at the first index of the buffer gets replaced with the new value:" << endl;
+    buffer.write(420);
+    printBuffer(buffer);
+}
+
