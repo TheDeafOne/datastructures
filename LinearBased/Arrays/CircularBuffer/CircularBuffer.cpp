655fa7b76f9558751b9e5337370d09c363ff6de4
TheDeafOne <woodburnkb20@gcc.edu>
2022-05-18 19:03:19 -0400

    CircularBuffer Tutorial

--- LinearBased/Arrays/CircularBuffer/CircularBuffer.cpp
+++ LinearBased/Arrays/CircularBuffer/CircularBuffer.cpp
@@ -16,12 +16,24 @@ CircularBuffer::CircularBuffer(size_t size) {
     buffer = (int*) calloc(size, sizeof(int)); 
 }
 
+/**
+ * @brief write sets the current index in the buffer to the given value and increments the writer
+ * if it reaches the end of the buffer, it circles back
+ * 
+ * @param value the value to be added to the buffer
+ */
 void CircularBuffer::write(int value) {
     buffer[writer++] = value; // set position at writer index to value and increment writer
     writer %= buffer_size; // set writer to wrap around to first index once it reaches the end of the buffer
 }
 
+/**
+ * @brief read gets the value at the current index and increments the reader
+ * 
+ * @return int the value being read from the buffer
+ */
 int CircularBuffer::read() {
+    cout << reader;
     int value = buffer[reader++]; // get value in a temp variable and increment reader
     reader %= buffer_size; // set reader to wrap around to first index once it reaches the end of the buffer
     return value;
