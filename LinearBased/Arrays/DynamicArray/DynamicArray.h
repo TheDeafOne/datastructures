d01b408ff76f1692c575b6ecfb2f228253539608
TheDeafOne <woodburnkb20@gcc.edu>
2022-05-20 23:58:36 -0400

    DynamicArray header methods filled out

--- LinearBased/Arrays/DynamicArray/DynamicArray.h
+++ LinearBased/Arrays/DynamicArray/DynamicArray.h
@@ -2,12 +2,21 @@
 #ifndef DYNAMICARRAY_H_
 #define DYNAMICARRAY_H_
 
-class BitArray {
+class DynamicArray {
     private:
-       
-
+        const int GROWTH_FACTOR = 2; // some use 1.5
+        int array_size;
+        int add_index;
+        int* array;
+        void expand();
+        
     public:
-       
+        DynamicArray();
+        int get(int idx);
+        void set(int idx, int value);
+        void add(int value);
+        void insert(int idx, int value);
+        int remove(int idx);
 };
 
 #endif
