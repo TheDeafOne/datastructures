e490f86f416206bfd48fbc8bb57d6c138c49079d
TheDeafOne <woodburnkb20@gcc.edu>
2022-05-22 17:47:48 -0400

    GapBuffer partial boilerplate

--- LinearBased/Arrays/GapBuffer/GapBuffer.h
+++ LinearBased/Arrays/GapBuffer/GapBuffer.h
@@ -4,8 +4,14 @@
 
 class GapBuffer {
     private:
+        static const int GROWTH_FACTOR = 2;
+        char* buffer;
+        int buffer_size;
+        int gap_size;
         
     public:
+       int bufferSize();
+       int gapSize();
        
 };
 
