3c50a80d6b3ccde6fc18f5849d732c9d1b9c81b7
TheDeafOne <woodburnkb20@gcc.edu>
2022-05-23 22:06:07 -0400

    HashedArrayTree broken logic

---
+++ LinearBased/Arrays/HashedArrayTree/HashedArrayTree.h
@@ -0,0 +1,43 @@
+// -lstdc++
+#ifndef HASHEDARRAYTREE_H_
+#define HASHEDARRAYTREE_H_
+
+class HashedArrayTree {
+    private:
+        // size of array from end to end
+        int array_size;
+        int leaf_size;
+        // current power of 2 that root array is at
+        int power;
+        // root array containing leaf arrays
+        int** root_array;
+        // index at which next leaf array will be added
+        int leaf_index;
+        // next index to add given value
+        int add_index;
+                
+        void expand();
+
+        void newLeaf(int** array, int* array_values = {}, int array_size = 0);
+
+    public:
+       HashedArrayTree();
+
+       int get(int idx);
+
+       int set(int idx, int value);
+
+       void add(int value);
+       
+       void insert(int idx, int value);
+
+       int remove(int idx);
+
+       int size() { return array_size; }
+
+       int rootSize() { return power; }
+
+       
+};
+
+#endif
