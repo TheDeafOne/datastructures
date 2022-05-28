78a827962b87b9b89f9a67f0f355dc7bf283a0be
TheDeafOne <woodburnkb20@gcc.edu>
2022-05-28 18:25:54 -0400

    LinkedList documentation

--- LinearBased/Lists/LinkedList/LinkedList.cpp
+++ LinearBased/Lists/LinkedList/LinkedList.cpp
@@ -2,28 +2,42 @@
 #include "LinkedList.h"
 using namespace std;
 
-/**
- * @brief Construct a new Linked List object
- * 
- */
+
 LinkedList::LinkedList() {
     list_size = 0;
     head = NULL;
     tail = NULL;
 }
 
-/**
- * @brief 
+ /**
+ * @brief get value from list using given index
  * 
- * @param value 
+ * @param idx index at which to get the node from the list
+ * @return int value found in the array at given index
+ */
+int LinkedList::get(int idx) {
+    Node* current_node = head;
+
+    // cycle through list until required node is reached
+    for (int i = 0; i < idx; i++) {
+        current_node = (*current_node).next;
+    }
+    return (*current_node).value;
+}
+
+/**
+ * @brief make a node with the given value and add it to the end of the list
+ * 
+ * @param value value to be added to the list
  */
 void LinkedList::add(int value) {
+    // handle the case where the list is empty
     if (list_size == 0) {
-        head = tail = new Node();
+        head = tail = new Node(); 
         (*head).value = value;
     } else {
         (*tail).next = new Node();
-        tail = (*tail).next;
+        tail = (*tail).next; // move tail to next node
         (*tail).value = value;
     }
 
@@ -31,48 +45,60 @@ void LinkedList::add(int value) {
 }
 
 /**
- * @brief 
+ * @brief set the node at the given index to the given value
  * 
- * @param idx 
- * @return int 
+ * @param idx index to set node value at
+ * @param value value to be set
+ * @return int value being replaced
  */
-int LinkedList::get(int idx) {
-    Node* tmp = head;
-    for (int i = 0; i < idx; i++) {
-        tmp = (*tmp).next;
-    }
-    return (*tmp).value;
-}
-
 int LinkedList::set(int idx, int value) {
-    Node* tmp = head;
+    Node* current_node = head;
+
+    // cycle through list until the required node is found
     for (int i = 0; i < idx; i++) {
-        tmp = (*tmp).next;
+        current_node = (*current_node).next;
     }
-    int val = (*tmp).value;
-    (*tmp).value = value;
+
+    int val = (*current_node).value; // return value
+    (*current_node).value = value; // set new value
     return val;
 }
 
+/**
+ * @brief insert given value at the given index in the list
+ * 
+ * @param idx index at which the given value should be inserted
+ * @param value to be inserted
+ */
 void LinkedList::insert(int idx, int value) {
-    Node* tmp = head;
+    Node* current_node = head;
     if (idx == 0) {
         head = new Node();
-        (*head).next =  tmp;
+        (*head).next =  current_node;
         (*head).value = value;
     } else {
+        // cycle through list until the node leading up to the required node is found
         for (int i = 0; i < idx - 1; i++) {
-            tmp = (*tmp).next;
-            cout << "tmp value: " << (*tmp).value << endl;
+            current_node = (*current_node).next;
         }
+        
+        // initialize the new node
         Node* new_node = new Node();
-        (*new_node).next = (*tmp).next;
         (*new_node).value = value;
-        (*tmp).next = new_node;  
+
+        // set new node and previous node pointers to insert new node into the list
+        (*new_node).next = (*current_node).next; 
+        (*current_node).next = new_node;  
     }
     list_size++;
 }
 
+/**
+ * @brief remove node from list at given index
+ * 
+ * @param idx index of node to be removed
+ * @return int value removed from array
+ */
 int LinkedList::remove(int idx) {
     Node* current_node = (*head).next; // current node pointer
     Node* previous_node = head; // previous node pointer
@@ -90,6 +116,7 @@ int LinkedList::remove(int idx) {
         previous_node = (*previous_node).next;
         current_node = (*current_node).next;
     }
+    
     // set the node pointers to pass over the required node, effectively removing it from the list
     (*previous_node).next = (*current_node).next;
     (*current_node).next =  NULL;
