e9b70deeeeb2600a029daf47a6cdf261eebbcf53
TheDeafOne <woodburnkb20@gcc.edu>
2022-05-28 17:29:28 -0400

    LinkedList set and insert logic

--- LinearBased/Lists/LinkedList/LinkedList.cpp
+++ LinearBased/Lists/LinkedList/LinkedList.cpp
@@ -2,12 +2,21 @@
 #include "LinkedList.h"
 using namespace std;
 
+/**
+ * @brief Construct a new Linked List object
+ * 
+ */
 LinkedList::LinkedList() {
     list_size = 0;
     head = NULL;
     tail = NULL;
 }
 
+/**
+ * @brief 
+ * 
+ * @param value 
+ */
 void LinkedList::add(int value) {
     if (list_size == 0) {
         head = tail = new Node();
@@ -21,6 +30,12 @@ void LinkedList::add(int value) {
     list_size++;
 }
 
+/**
+ * @brief 
+ * 
+ * @param idx 
+ * @return int 
+ */
 int LinkedList::get(int idx) {
     Node tmp = *head;
     for (int i = 0; i < idx; i++) {
@@ -29,6 +44,39 @@ int LinkedList::get(int idx) {
     return tmp.value;
 }
 
+int LinkedList::set(int idx, int value) {
+    Node* tmp = head;
+    for (int i = 0; i < idx; i++) {
+        tmp = (*tmp).next;
+    }
+    int val = (*tmp).value;
+    (*tmp).value = value;
+    return val;
+}
+
+void LinkedList::insert(int idx, int value) {
+    Node* tmp = head;
+    if (idx == 0) {
+        head = new Node();
+        (*head).next =  tmp;
+        (*head).value = value;
+    } else {
+        for (int i = 0; i < idx - 1; i++) {
+            tmp = (*tmp).next;
+            cout << "tmp value: " << (*tmp).value << endl;
+        }
+        Node* new_node = new Node();
+        (*new_node).next = (*tmp).next;
+        (*new_node).value = value;
+        (*tmp).next = new_node;  
+    }
+    list_size++;
+}
+
+/**
+ * @brief Construct a new Node object
+ * 
+ */
 Node::Node() {
     next = NULL;
     value = 0;
