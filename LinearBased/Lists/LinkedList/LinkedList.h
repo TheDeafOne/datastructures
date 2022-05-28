5d2746bf357500eb793b81fc401bd33aa3f50b58
TheDeafOne <woodburnkb20@gcc.edu>
2022-05-28 17:29:44 -0400

    LinkedList head documentation

--- LinearBased/Lists/LinkedList/LinkedList.h
+++ LinearBased/Lists/LinkedList/LinkedList.h
@@ -3,22 +3,74 @@
 #define LINKEDLIST_H_
 class Node {
     public:
-        Node* next;
-        int value;
-        Node();
+        Node* next; // pointer to next node
+        int value; // value that node contains
+
+        /**
+         * @brief Construct a new Node object
+         * 
+         */
+        Node(); 
 };
 
 class LinkedList {
     private:
-        Node* head;
-        Node* tail;
+        Node* head; // pointer to head node
+        Node* tail; // pointer to tail node
         int list_size;
+
     public:
+        /**
+         * @brief Construct a new Linked List object
+         * 
+         */
         LinkedList();
+
+        /**
+         * @brief get value from list using given index
+         * 
+         * @param idx index at which to get the node from the list
+         * @return int value found in the array at given index
+         */
         int get(int idx);
+
+        /**
+         * @brief make a node with the given value and add it to the end of the list
+         * 
+         * @param value value to be added to the list
+         */
         void add(int value);
+
+        /**
+         * @brief set the node at the given index to the given value
+         * 
+         * @param idx index to set node value at
+         * @param value value to be set
+         * @return int value being replaced
+         */
+        int set(int idx, int value);
+
+        /**
+         * @brief insert given value at the given index in the list
+         * 
+         * @param idx index at which the given value should be inserted
+         * @param value to be inserted
+         */
         void insert(int idx, int value);
+
+        /**
+         * @brief remove node from list at given index
+         * 
+         * @param idx index of node to be removed
+         * @return int value removed from array
+         */
         int remove(int idx);
+
+        /**
+         * @brief return size of list
+         * 
+         * @return int size of list
+         */
         int size() { return list_size; };
 };
 
